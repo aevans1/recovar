0697a5108ef8d5f6e96dfaa4c15c85022bed9ea9
aevans1 <levans@flatironinstitute.org>
2024-10-11 11:41:56 -0400

    spike dataset scripts

---
+++ analyze_spike_datasets.py
@@ -0,0 +1,179 @@
+import recovar.config
+from importlib import reload
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objs as go
+import plotly.offline as py
+from recovar.fourier_transform_utils import fourier_transform_utils
+import jax.numpy as jnp
+ftu = fourier_transform_utils(jnp)
+from recovar import image_assignment, noise
+from sklearn.metrics import confusion_matrix
+from recovar import simulate_scattering_potential as ssp
+from recovar import simulator, utils, image_assignment, noise, output, dataset
+import prody
+import pickle
+import argparse
+import sys
+reload(simulator)
+import scipy
+
+def normalize_weights(log_weights):
+    weighted_alphas = np.exp(log_weights)
+    weighted_alphas = weighted_alphas / np.sum(weighted_alphas)
+    return weighted_alphas
+
+def expectation_maximization_weights(log_Pij, log_weights_init=None, num_iterations=None):
+    """
+    This function updates the weights according to the expectation maximization
+     algorithm for mixture models.
+    """
+    num_images = log_Pij.shape[0]
+    if log_weights_init is None:
+        log_weights = np.zeros((1, log_Pij.shape[1]))
+        log_weights = np.log(np.array([[0.9, 0.1]]))
+    else:
+        log_weights = log_weights_init
+
+    log_weights = np.log(normalize_weights(log_weights))
+
+    norms = np.zeros(num_iterations)
+    loss = np.zeros(num_iterations)
+    for k in range(num_iterations):
+        log_likelihood_per_image = scipy.special.logsumexp(log_Pij + log_weights, axis=1)
+        log_weighted_likelihoods = log_Pij + log_weights
+        log_posteriors = log_weighted_likelihoods - log_likelihood_per_image.reshape(
+            log_likelihood_per_image.shape[0], 1
+        )
+        log_weights = scipy.special.logsumexp(log_posteriors - np.log(num_images), axis=0)
+
+        # compute two parameters to monitor for convergence: norm of weights, and log marginal likelihood
+        norms[k] = np.linalg.norm(normalize_weights(log_weights))
+        loss[k] = -1*(1/num_images)*np.sum(log_likelihood_per_image)
+    log_weights = np.log(normalize_weights(log_weights))
+    return log_weights, norms, loss
+
+def classify_with_prior(pop, log_likelihoods, true_assignments):
+
+    log_prior =  np.log(np.expand_dims(pop, axis=1)).T
+    assignments = jnp.argmax(log_likelihoods + log_prior , axis = 1)
+
+    confus = confusion_matrix(assignments, true_assignments)
+    if confus.size > 1:
+        error_observed = (confus[1,0] + confus[0,1] ) / assignments.size
+    else:
+        error_observed = 0
+    return error_observed
+
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_folder", default="/mnt/home/levans/ceph/spike/recovar_experiments_100k", type=str)
+
+    args = parser.parse_args()
+    output_folder = args.output_folder
+
+    # Load in things
+    file = open(output_folder + '/' + 'noise_levels.pkl','rb')
+    noise_levels = pickle.load(file)
+    file.close()  
+
+    file = open(output_folder + '/' + 'pops_errors.pkl','rb')
+    pops_errors = pickle.load(file)
+    error_observed = pops_errors["error_observed"]
+    error_predicted = pops_errors["error_predicted"]
+    deconvolve_pop = pops_errors["deconvolve_pop"]
+    deconvolve_pop_alt = pops_errors["deconvolve_pop_alt"]
+    observed_pop_soft = pops_errors["observed_pop_soft"]
+    observed_pop = pops_errors["observed_pop"]
+    file.close()  
+
+    volume_distribution = np.array([0.8, 0.2])
+   
+    reweight_pop = np.zeros((len(noise_levels), 2))
+    bayes_observed = np.zeros(len(noise_levels))
+    deconvolve_observed = np.zeros(len(noise_levels))
+    reweight_observed = np.zeros(len(noise_levels))
+
+    for idx, noise_level in enumerate(noise_levels):
+
+        dataset_folder = output_folder + '/' + f'dataset{idx}/'
+        print(f"Starting at noise level {idx} of {len(noise_levels)}") 
+
+        # Load in stats
+        file = open(dataset_folder + '/' + 'likelihoods_assignments.pkl','rb')
+        likelihoods_assignments = pickle.load(file)
+        file.close()  
+
+        log_likelihoods = likelihoods_assignments['log_likelihoods']
+        hard_assignments = likelihoods_assignments['hard_assignments']
+        true_assignments = likelihoods_assignments['true_assignments']
+
+        # Get likelihood weights
+        log_weights, norms, loss = expectation_maximization_weights(log_likelihoods, num_iterations=5000)  
+        reweight_pop[idx, :] = np.exp(log_weights)
+        print('cryoER weights:', reweight_pop[idx, :])
+        print('deconv weights:', deconvolve_pop[idx, :])
+
+        deconvolve_observed[idx] =  classify_with_prior(deconvolve_pop[idx, :], log_likelihoods, true_assignments)
+        reweight_observed[idx] =  classify_with_prior(reweight_pop[idx, :], log_likelihoods, true_assignments)
+        bayes_observed[idx] =  classify_with_prior(volume_distribution, log_likelihoods, true_assignments)
+
+        # Dump results to file
+        extra_stats = {'deconvolve_observed' : deconvolve_observed, \
+                       'reweight_observed' : reweight_observed, \
+                       'bayes_observed' : bayes_observed, \
+                       'reweight_pop' : reweight_pop, \
+                        } 
+        recovar.utils.pickle_dump(extra_stats, output_folder + '/' + 'extra_stats.pkl')
+        
+        # Dump results to file
+        expec_maxim_stats = {'loss' : loss, \
+                       'norms' : norms} 
+        recovar.utils.pickle_dump(expec_maxim_stats, dataset_folder + '/' + 'expec_maxim_stats.pkl')
+
+        # Make a plot each time.
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(10, 6))
+        plt.semilogx(noise_levels[:idx+1], error_predicted[:idx+1], label='Hard Assign, Analytical', color='k', marker='o', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], error_observed[:idx+1], label='Hard Assign', color='blue', marker='s', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], deconvolve_observed[:idx+1], label='Deconvolve', color='green', marker='s', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], reweight_observed[:idx+1], label='Reweight', color='purple', marker='s', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], bayes_observed[:idx+1], label='Bayes Optimal', color='orange', marker='s', markersize=6, linewidth=2)
+
+
+        plt.xlabel('Noise Level', fontsize=14)
+        plt.ylabel('Misclassification Rate', fontsize=14)
+        plt.title('Misclassification Rate vs. Noise Level', fontsize=16)
+ 
+        plt.legend(fontsize=12)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=12)
+        plt.tight_layout()
+        plt.savefig(output_folder + '/' + 'curve_extra.png')
+
+        plt.figure(figsize=(10, 6))
+        plt.semilogx(noise_levels[:idx+1], observed_pop[:idx+1, 0], label='Hard Assign', color='blue', marker='o', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], observed_pop_soft[:idx+1, 0], label='Soft Assign', color='orange', marker='o', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], deconvolve_pop[:idx+1, 0], label='Deconvolve', color='green', marker='s', markersize=6, linewidth=2)
+        #plt.semilogx(noise_levels[:idx+1], deconvolve_pop_alt[:idx+1, 0], label='Deconvolve_alt', color='red', marker='s', markersize=6, linewidth=2)
+        plt.semilogx(noise_levels[:idx+1], reweight_pop[:idx+1, 0], label='Ensemble Reweight', color='purple', marker='s', markersize=6, linewidth=2)
+        plt.hlines(y=0.8, xmin=noise_levels[0], xmax=noise_levels[-1], label="True % Population", linestyle="--", color="k", linewidth=3.0)
+
+        plt.xlabel('Noise Level', fontsize=14)
+        plt.ylabel('% Population in state 1', fontsize=14)
+        plt.legend(fontsize=12)
+        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+        plt.xticks(fontsize=12)
+        plt.yticks(fontsize=12)
+        plt.tight_layout()
+        plt.savefig(output_folder + '/' + 'populations_extra.png')
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+    print("Done")
--- make_spike_datasets.py
+++ make_spike_datasets.py
@@ -15,22 +15,34 @@ from recovar import simulator, utils, image_assignment, noise, output, dataset
 import prody
 reload(simulator)
 
+import argparse
+import sys
+
+
+
 def main():
-    ## CHANGE THESE FOLDERS
-    output_folder = '/home/mg6942/mytigress/hard_assignment_exp/'
-    pdb_folder = './'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_images", default=10000, type=int)
+    parser.add_argument("--output_folder", default="/mnt/home/levans/ceph/spike/recovar_experiments", type=str)
+    parser.add_argument("--pdb_folder", default="/mnt/home/levans/ceph/spike/spike_models_for_bad_histogram/all_atom/without_glycans", type=str)
+
+    args = parser.parse_args()
+    n_images = args.n_images
+    output_folder = args.output_folder
+    pdb_folder = args.pdb_folder
+    output_folder = args.output_folder
+    pdb_folder = args.pdb_folder
 
     # Some parameters to set
-    n_images = 100000
     grid_size = 256
     Bfactor = 60 
-    noise_level_tests = np.logspace(1,6,20) # these seem reasonble.
-    # This would produce 2 million images of size 256x256
+    noise_levels = np.logspace(-1,4,10)
 
     ## Make volumes from PDB
-    pdbs = [ '3down_nogly.pdb', 'up_nogly.pdb']
+    pdbs = [ '3down_nogly.pdb', '1up_3drest_nogly.pdb']
 
-    voxel_size =1.3 * 256 / grid_size
+    voxel_size = 1.3 * 256 / grid_size
     volume_shape = tuple(3*[grid_size])
 
     # Center atoms (but shift by same amount)
@@ -41,7 +53,6 @@ def main():
     # coords = coords - offset
     for atoms in pdb_atoms:
         atoms.setCoords(atoms.getCoords() - offset)
-
         
     ## Make B-factored volumes (will be considered g.t.)     
     Bfaced_vols = len(pdbs)*[None]
@@ -54,97 +65,38 @@ def main():
     # disc_type_sim = 'linear_interp'
     # disc_type_infer = 'linear_interp'
 
-
-    volume_folder = output_folder + 'true_volumes/'
+    volume_folder = output_folder + '/' + 'true_volumes/'
     output.mkdir_safe(volume_folder)
     output.save_volumes( Bfaced_vols, volume_folder, from_ft= True)
 
-    # plt.imshow(ftu.get_idft3(Bfaced_vols[0].reshape(volume_shape)).sum(axis=0).real)
-    # plt.figure()
-    # plt.imshow(ftu.get_idft3(Bfaced_vols[1].reshape(volume_shape)).sum(axis=0).real)
-    
+    for idx, noise_level in enumerate(noise_levels):
 
-    error_observed = np.zeros(noise_level_tests.size)
-    error_predicted= np.zeros(noise_level_tests.size)
-
-    for idx, noise_level in enumerate(noise_level_tests):
-        
+        print(f"Starting at noise level {idx} of {len(noise_levels)}") 
         # Generate dataset
         volume_distribution = np.array([0.8,0.2])
         noise_level = noise_level
-        dataset_folder = output_folder + f'/dataset{idx}/'
+        dataset_folder = output_folder + '/' + f'dataset{idx}/'
         image_stack, sim_info = simulator.generate_synthetic_dataset(dataset_folder, voxel_size, volume_folder, n_images,
             outlier_file_input = None, grid_size = grid_size,
             volume_distribution = volume_distribution,  dataset_params_option = "uniform", noise_level = noise_level,
             noise_model = "white", put_extra_particles = False, percent_outliers = 0.00, 
             volume_radius = 0.7, trailing_zero_format_in_vol_name = True, noise_scale_std = 0, contrast_std = 0, disc_type = disc_type_sim)
         
-        # gt_volumes = np.array(Bfaced_vols) * sim_info['scale_vol']
-
         # Load datasets and volumes
         # Volumes are scaled so that images are normalized. So they have a slightly different scale for each dataset.
         volumes = simulator.load_volumes_from_folder(sim_info['volumes_path_root'], sim_info['grid_size'] , sim_info['trailing_zero_format_in_vol_name'], normalize=False )
         gt_volumes = volumes * sim_info['scale_vol']
 
-        
         dataset_options = dataset.get_default_dataset_option()
-        dataset_options['particles_file'] = dataset_folder + f'particles.{grid_size}.mrcs'
-        dataset_options['ctf_file'] = dataset_folder + f'ctf.pkl'
-        dataset_options['poses_file'] = dataset_folder + f'poses.pkl'
-        cryo = dataset.load_dataset_from_dict(dataset_options, lazy = False)
+        dataset_options['particles_file'] = dataset_folder + '/' + f'particles.{grid_size}.mrcs'
+        dataset_options['ctf_file'] = dataset_folder + '/' + f'ctf.pkl'
+        dataset_options['poses_file'] = dataset_folder + '/' + f'poses.pkl'
         
-        # Compute hard-assignment
-        batch_size = 1000
-        image_cov_noise = np.asarray(noise.make_radial_noise(sim_info['noise_variance'], cryo.image_shape))
-        log_likelihoods = image_assignment.compute_image_assignment(cryo, gt_volumes,  image_cov_noise, batch_size, disc_type = disc_type_infer)
-        assignments = jnp.argmin(log_likelihoods, axis = 0)
-        
-        confus = confusion_matrix(assignments, sim_info['image_assignment'])
-        
-        # Compute the gamma from the note.
-        
-        if confus.size > 1:
-            error_observed[idx] = (confus[1,0] + confus[0,1] ) / assignments.size
-        else:
-            error_observed[idx] = 0
-        
-        error_predicted[idx] = image_assignment.estimate_false_positive_rate(cryo, gt_volumes,  image_cov_noise, batch_size, disc_type = disc_type_infer)
-        print('o', error_observed)
-        print('p', error_predicted)
-        
-        # Checking with the deconvolution formula gives
-        observed_pop = np.array([np.mean(assignments==0), np.mean(assignments==1)])
-        deconvolve_matrix = np.array( [ [1- error_predicted[idx], error_predicted[idx]], [error_predicted[idx], 1- error_predicted[idx]] ])
-        print('Observed pop:', observed_pop)
-        print('Deconvolve mat:', deconvolve_matrix)
-        print('Deconvolved pop:', np.linalg.solve(deconvolve_matrix, observed_pop))
-
         # Dump results to file
-        result = { 'log_llh': log_likelihoods, 'hard_assignment' : assignments, 'true_assignment' : sim_info['image_assignment'] , 'predicted_error_rate' : error_predicted[idx]   } 
-        recovar.utils.pickle_dump( result, dataset_folder + 'result.pkl')
-        recovar.utils.pickle_dump( { 'error_observed' : error_observed, \
-                                    'error_predicted' : error_predicted, 'noise_level_tests' : noise_level_tests },
-                                output_folder + 'curve.pkl') 
+        recovar.utils.pickle_dump( sim_info, dataset_folder + '/' + 'sim_info.pkl')
 
-        # Make a plot each time.
-        import matplotlib.pyplot as plt
-
-        plt.figure(figsize=(10, 6))
-        plt.semilogx(noise_level_tests[:idx+1], error_predicted[:idx+1], '-o', label='Analytical', color='blue', marker='o', markersize=6, linewidth=2)
-        plt.semilogx(noise_level_tests[:idx+1], error_observed[:idx+1], '-s', label='Observed', color='green', marker='s', markersize=6, linewidth=2)
-
-        plt.xlabel('Noise Level', fontsize=14)
-        plt.ylabel('False Positive Rate', fontsize=14)
-        plt.title('False Positive Rate vs. Noise Level', fontsize=16)
-        plt.legend(fontsize=12)
-        plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-        plt.xticks(fontsize=12)
-        plt.yticks(fontsize=12)
-        plt.tight_layout()
-        plt.savefig(output_folder + 'curve.png')
-        plt.show()
-
-        
+    # Save info relevant to all datasets
+    recovar.utils.pickle_dump( noise_levels, output_folder + '/' + 'noise_levels.pkl')
 
 if __name__ == '__main__':
     main()
