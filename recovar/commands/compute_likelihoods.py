24d42e010bc68a8bf4c1fee657c09436194a6535
aevans1 <levans@flatironinstitute.org>
2026-05-28 20:20:19 -0400

    linted

--- recovar/commands/compute_likelihoods.py
+++ recovar/commands/compute_likelihoods.py
@@ -1,8 +1,12 @@
+import argparse
 import logging
+import os
+
 import jax.numpy as jnp
-from recovar.output import output as o
+
 from recovar.heterogeneity import latent_density as ld
-import os, argparse
+from recovar.output import output as o
+
 logger = logging.getLogger(__name__)
 
 def parse_args():
@@ -22,18 +26,18 @@ def compute_likelihoods_2d_grid(recovar_result_dir, output_dir, percentile_rejec
     po = o.PipelineOutput(path)
 
     ## Here, by default using nonregularized PC's, like the recovar paper, and throwing away unstable covariances
-    if reg: 
+    if reg:
         zs = po.get_embedding_component("latent_coords", 2)
         cov_zs = po.get_embedding_component("latent_precision", 2)
-    else: 
+    else:
         zs = po.get_embedding_component("latent_coords_noreg", 2)
         cov_zs= po.get_embedding_component("latent_precision_noreg", 2)
 
-    ## Throwing away unstable covariances 
+    ## Throwing away unstable covariances
     cov_zs_norm = jnp.linalg.norm(cov_zs, axis=(-1,-2), ord = 2)
     good_zs = cov_zs_norm > jnp.percentile(cov_zs_norm, percentile_reject)
     zs = zs[good_zs][:,:2]
-    cov_zs = cov_zs[good_zs][:,:2, :2] 
+    cov_zs = cov_zs[good_zs][:,:2, :2]
 
     ## Making a grid
     latent_space_bounds = ld.compute_latent_space_bounds(zs, percentile)
@@ -49,25 +53,25 @@ def compute_likelihoods_2d_grid(recovar_result_dir, output_dir, percentile_rejec
 def compute_likelihoods_point_cloud(recovar_result_dir, output_dir, zdim=2, percentile_reject=10, reg=False):
     path =  os.path.abspath(recovar_result_dir + '/')
     po = o.PipelineOutput(path)
-    
+
     ## Here, by default using nonregularized PC's, like the recovar paper, and throwing away unstable covariances
-    if reg: 
+    if reg:
         zs = po.get_embedding_component("latent_coords", 2)
         cov_zs = po.get_embedding_component("latent_precision", 2)
-    else: 
+    else:
         zs = po.get_embedding_component("latent_coords_noreg", 2)
         cov_zs= po.get_embedding_component("latent_precision_noreg", 2)
 
-    ## Throwing away unstable covariances 
+    ## Throwing away unstable covariances
     cov_zs_norm = jnp.linalg.norm(cov_zs, axis=(-1,-2), ord = 2)
     good_zs = cov_zs_norm > jnp.percentile(cov_zs_norm, percentile_reject)
 
     zs = zs[good_zs][:,:zdim]
-    cov_zs = cov_zs[good_zs][:,:zdim, :zdim] 
+    cov_zs = cov_zs[good_zs][:,:zdim, :zdim]
 
     ## Compute full likelihood matrix
     #log_likelihood_zs = ld.compute_latent_log_likelihood(zs, zs, cov_zs)
-    
+
     ## Compute likelihood matrix for subset of z nodes, still all images, for easier computation
     #zs_subset = zs[::10, :]
     #log_likelihood_zs_subset = ld.compute_latent_log_likelihood(zs_subset, zs, cov_zs)
@@ -81,17 +85,17 @@ def compute_likelihoods_point_cloud(recovar_result_dir, output_dir, zdim=2, perc
     #np.save(f"{output_dir}/log_likelihood_zs_{zdim}.npy", log_likelihood_zs)
     #np.save(f"{output_dir}/log_likelihood_zs_subset_{zdim}.npy", log_likelihood_zs_subset)
     #np.save(f"{output_dir}/log_likelihood_zs_subset_larger_{zdim}.npy", log_likelihood_zs_subset_larger)
-    
+
     ## Save the pc embeddings
     #np.save(f"{output_dir}/zs_zdim_{zdim}.npy", zs)
     #np.save(f"{output_dir}/zs_subset_{zdim}.npy", zs_subset)
     #np.save(f"{output_dir}/zs_subset_larger_{zdim}.npy", zs_subset_larger)
 
-    ## Save covariances, for diagnostics 
+    ## Save covariances, for diagnostics
     #np.save(f"{output_dir}/cov_zs_{zdim}.npy", cov_zs)
 
-    
+
 if __name__ == "__main__":
     args = parse_args()
-    compute_likelihoods_2d_grid(recovar_result_dir=args.recovar_result_dir, 
+    compute_likelihoods_2d_grid(recovar_result_dir=args.recovar_result_dir,
                                 output_dir=args.output_dir)
