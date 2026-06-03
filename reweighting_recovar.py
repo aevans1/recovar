47ebf39a1b7b5f8b8947ff9cd9da1d9b43e76651
aevans1 <levans@flatironinstitute.org>
2026-06-03 11:44:06 -0400

    trying plotting and speeding up more

--- reweighting_recovar.py
+++ reweighting_recovar.py
@@ -1,8 +1,11 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
+import equinox as eqx
 import matplotlib.pyplot as plt
 
 import os
+import tqdm
 
 from recovar.heterogeneity import latent_density as ld
 from recovar.output import output as o
@@ -10,12 +13,60 @@ from recovar.output import output as o
 
 XLA_PYTHON_CLIENT_PREALLOCATE=False
 
-@jax.jit
-def compute_grad(weights, zs, cov_zs):
-    #model = likelihood @ weights
-    #grad = jnp.mean(likelihood/model[:, jnp.newaxis], axis=0)
-    return None
 
+
+#for i in tqdm(range(0, len(dataset), batch_size)):
+#    batch_indices = np.arange(i, min(i + batch_size, len(dataset)))
+#    particle_stack = dataset[batch_indices]
+#    particle_stack = normalize_stack(particle_stack, whitening_filter)
+#
+#    for i in range(len(volumes)):
+#        log_likelihood_matrix[batch_indices, i] = compute_likelihoods(
+#            volumes[i],
+#            particle_stack,
+#            dilated_mask,
+#            image_sign,
+#            whitening_filter,
+#        )
+
+@jax.jit
+def normalize_log_likeli_to_likeli(log_likelihood):
+    """
+    Subtracts the largest entry from each row of  the log likelihood.
+    This is for stability, before transforming to likelihood (like in a soft-max) 
+    The gradient is invariant to row scaling of likelihood, so this is valid.
+    With this normalizing, we avoid working in log space for the grad and loss.
+
+    Parameters
+    ----------
+    log_likelihood : jax.Array
+        log_likelihood of data-point i from node j.
+        must be of shape (num_data x num_nodes) 
+
+    Returns
+    -------
+    likelihood: jax.Array
+    """
+    log_likelihood -= jnp.amax(log_likelihood, axis=1)[:, None]
+    likelihood = jnp.exp(log_likelihood)
+    return likelihood
+
+
+@eqx.filter_jit
+def _compute_grad_batch(weights, zs_batch, cov_zs_batch, zs_grid):
+    """JIT-compiled gradient computation, in batches"""
+    log_likelihood_zs_batch = -1*ld.compute_latent_log_likelihood(zs_grid, zs_batch, cov_zs_batch)
+    likelihood_zs_batch = normalize_log_likeli_to_likeli(log_likelihood_zs_batch)
+    denominator = likelihood_zs_batch @ weights 
+    return jnp.sum(likelihood_zs_batch / denominator[:, None], axis=0)
+
+def compute_grad(weights, zs, cov_zs, zs_grid, batch_size=100):
+    grad = jnp.zeros(weights.shape[0])
+    for i in range(0, len(zs), batch_size):
+        end_idx = min(i + batch_size, zs.shape[0])
+        grad += _compute_grad_batch(weights, zs[i:end_idx], cov_zs[i:end_idx], zs_grid)
+    grad /= len(zs)
+    return grad
 
 @jax.jit
 def update_weights(weights, grad):
@@ -70,12 +121,12 @@ def online_multiplicative_gradient(
     path =  os.path.abspath(recovar_result_dir + '/')
     po = o.PipelineOutput(path)
 
-    zs = po.get_embedding_component("latent_coords_noreg", 2)
-    cov_zs= po.get_embedding_component("latent_precision_noreg", 2)
+    zs = po.get_embedding_component("latent_coords_noreg", zdim)
+    cov_zs= po.get_embedding_component("latent_precision_noreg", zdim)
 
     ## Throwing away unstable covariances
     cov_zs_norm = jnp.linalg.norm(cov_zs, axis=(-1,-2), ord = 2)
-    good_zs = cov_zs_norm > jnp.percentile(cov_zs_norm, percentile_reject=10)
+    good_zs = cov_zs_norm > jnp.percentile(cov_zs_norm, q=10)
     zs = zs[good_zs][:,:zdim]
     cov_zs = cov_zs[good_zs][:,:zdim, :zdim]
 
@@ -101,13 +152,13 @@ def online_multiplicative_gradient(
     #likelihood = normalize_log_likeli_to_likeli(log_likelihood)
   
     # Initialize scaling for gap stopping criteria
-    gap_scale = scaled_gap(compute_grad(zs, cov_zs, nodes), weights, scale=1.0)
+    gap_scale = scaled_gap(compute_grad(weights, zs, cov_zs, nodes), weights, scale=1.0)
 
     reached_gap = False
     for k in range(max_iterations):
         # Update grad
-        grad = compute_grad(zs, cov_zs, nodes)
-
+        grad = compute_grad(weights, zs, cov_zs, nodes)
+        print(k)
         # Check stopping criterions
         gap = scaled_gap(grad, weights, gap_scale)
 
@@ -126,6 +177,72 @@ def online_multiplicative_gradient(
         weights = update_weights(weights, grad)
 
     return weights
+
+def plot_density(density, function=None, cmap="inferno"):
+    from recovar.output.output import sum_over_other
+
+    def half_slice_other(density, axes):
+        axes = [i for i in range(density.ndim) if i not in axes]
+        axes = np.sort(axes)
+        for i in range(len(axes) - 1, -1, -1):
+            density = np.take(density, density.shape[0] // 2, axis=axes[i])
+        return density
+
+    function = half_slice_other if function == "slice" else function
+    function = sum_over_other if function is None else function
+
+    plt.rcParams.update({})
+    font = {"weight": "bold", "size": 22}
+    import matplotlib
+
+    matplotlib.rc("font", **font)
+
+    n_plots = 2
+    n_cols = density.ndim + 1 if density.ndim < 2 else density.ndim
+    fig, axs = plt.subplots(n_plots, n_cols, figsize=(n_cols * 5, n_plots * 5))
+    print(axs.shape)
+    global is_first
+    is_first = True
+
+    def plot_dens(density, title, n_plot, first=False):
+        density = np.asarray(density)
+        global is_first
+        if density.ndim == 1:
+            axs[n_plot, 0].plot(density)
+            axs[n_plot, 0].set_title(title)
+            axs[n_plot, 0].set_xticklabels([])
+            axs[n_plot, 0].set_yticklabels([])
+            if is_first:
+                axs[n_plot, 0].set_title(f"PC x={0}")
+            axs[n_plot, 0].set_ylabel(title)
+            return
+
+        for k in range(1, density.ndim):
+            if k == 1:
+                axs[n_plot, k - 1].set_ylabel(title)
+            axs[n_plot, k - 1].set_xticklabels([])
+            axs[n_plot, k - 1].set_yticklabels([])
+
+            to_plot = function(density, [0, k])
+            axs[n_plot, k - 1].imshow(to_plot.T, cmap=cmap)
+            if is_first:
+                axs[n_plot, k - 1].set_title(f"PC x={0}, y={k}")
+
+        if density.ndim > 2:
+            to_plot = function(density, [1, 2])
+            axs[n_plot, k].imshow(to_plot.T, cmap=cmap)
+            axs[n_plot, k].set_xticklabels([])
+            axs[n_plot, k].set_yticklabels([])
+            if is_first:
+                axs[n_plot, k].set_title(f"PC x={1}, y={2}")
+        is_first = False
+
+
+    plot_dens(density, "MG density", 0)
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+
+
 
 def main():
     # Set up RNG keys
@@ -147,9 +264,9 @@ def main():
     #print(grid.shape)
 
     # Load likelihood matrix
-    log_likelihood = -1*jnp.load("/mnt/home/levans/ceph/archive/cryo_reweighting_examples/latent_example/log_likelihood_zs_grid_zdim_2.npy").astype('float16')
-    num_data, num_nodes = jnp.shape(log_likelihood)
-    print(log_likelihood.shape)
+    #log_likelihood = -1*jnp.load("/mnt/home/levans/ceph/archive/cryo_reweighting_examples/latent_example/log_likelihood_zs_grid_zdim_2.npy").astype('float16')
+    #num_data, num_nodes = jnp.shape(log_likelihood)
+    #print(log_likelihood.shape)
 
     ##true_weights = jnp.load(f"{data_dir}/hsp90_true_weights.npy")
     #weights, info = opt.multiplicative_gradient(log_likelihood, 
@@ -160,16 +277,28 @@ def main():
     #                                                train_test=False,
     #                                                diagnostic=False)
     #this code will plot `weights` returned above as the max iteration weights in the plots: 
-    weights, info = online_multiplicative_gradient(recovar_result_dir,
-                                                   zdim=2,
+    recovar_result_dir="/mnt/home/levans/ceph/recovar_testing/bad_histogram_igg/_given_mask_with_correct_contrast"
+    
+    zdim=2
+    #weights = online_multiplicative_gradient(recovar_result_dir,
+    #                                               zdim=2
+    #)
+    weights = online_multiplicative_gradient(recovar_result_dir,
+                                             zdim=zdim,
+                                             max_iterations=10,
+                                             tol=-1
     )
-    #this code will plot `weights` returned above as the max iteration weights in the plots: 
+    
+    jnp.savez(f"weights_online_zdim_{zdim}.npy", weights) 
+    
+    if zdim == 2: 
+        plt.imshow(weights.reshape(200,200).T, cmap="magma")
+        plt.savefig(f"plot_fig_weights_online_{zdim}.png",dpi=300) 
 
-    
-    
-    jnp.savez("weights.npy", weights) 
-    plt.imshow(weights.reshape(200,200).T, cmap="magma")
-    plt.savefig("plot_fig.png",dpi=300) 
+    if zdim == 4: 
+        plot_density(weights.reshape(50,50,50,50)) 
+        plt.savefig("plot_density_recovar_style.png",dpi=300) 
+        plt.savefig("plot_density_recovar_style_4D.png",dpi=300) 
     plt.show()
 
 
