a127374d512fe4f5dc19e1b4e8924cbfe3020485
aevans1 <levans@flatironinstitute.org>
2026-08-15 15:50:24 -0400

    error in utils import

--- recovar/commands/estimate_conformational_density.py
+++ recovar/commands/estimate_conformational_density.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
-from recovar.utils import utils
+from recovar import utils
 from recovar.heterogeneity import deconvolve_density
 from recovar.output import output
 
--- recovar/commands/estimate_conformational_density_alt.py
+++ recovar/commands/estimate_conformational_density_alt.py
@@ -5,9 +5,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
+from recovar import utils
 from recovar.heterogeneity import calibrate_density
 from recovar.output import output
-from recovar.utils import utils
 
 logger = logging.getLogger(__name__)
 
